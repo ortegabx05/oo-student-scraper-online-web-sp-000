cbc5ec9d1dde40b75136a373a9b309b8b109ba33
jessi ortega <ortegabx05@gmail.com>
2019-08-29 17:54:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,28 +5,28 @@ require 'nokogiri'
 class Scraper
 
 
-   def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url)
 
       doc = Nokogiri::HTML(open(index_url))
       student_list = []
 
-        doc.css('.student-card').each do |students|
+      doc.css('.student-card').each do |students|
           name = students.css('.student-name').text
           location = students.css('.student-location').text
           profile_url = students.css('a').attribute('href').value
           student = {name: name, location: location, profile_url: profile_url}
           student_list << student
-      end
+     end
       student_list
-    end
+  end
 
 
-   def self.scrape_profile_page(profile_url)	  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(profile_url)
 
      doc = Nokogiri::HTML(open(profile_url))
-  student = {}
+     student = {}
 
-    social_container = doc.css(".social-icon-container a").collect {|icon| icon.attribute("href").value}
+      social_container = doc.css(".social-icon-container a").collect {|icon| icon.attribute("href").value}
 
       social_container.each do |link|
       if link.include?("twitter")
@@ -37,14 +37,12 @@ class Scraper
         student[:github] = link
       else
         student[:blog] = link
-      end
-    end
+     end
+  end
 
       student[:bio] = doc.css('.description-holder p').text
     student[:profile_quote] = doc.css('.profile-quote').text
   student
-  end	  end
-
-
- end	end
+ end
+end
 
