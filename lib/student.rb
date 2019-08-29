ae1cea2168bbd133804889294886f8e66973f5d2
jessi ortega <ortegabx05@gmail.com>
2019-08-29 17:56:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
-     student_hash.each {|key, value| self.send(("#{key}="), value)}
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
     @@all << self
   end
 
